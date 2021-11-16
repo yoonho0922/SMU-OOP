64cf642ab9c6d265641adf986e677e03f0ef5210
Yoonho Ahn <yoonho5684@gmail.com>
2021-11-16 11:25:07 +0900

    hw05 rename

--- src/Homework05.java
+++ src/Homework05.java
@@ -2,7 +2,7 @@ import hw05.Computer;
 import hw05.Monitor;
 import hw05.PersonalComputer;
 
-public class Homework5 {
+public class Homework05 {
     public static void main(String[] args) {
         Computer computer = new Computer("Core i7",
                 "16GB", "2TB", "White", "500W");
